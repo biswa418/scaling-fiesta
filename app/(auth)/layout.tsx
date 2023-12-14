1de725394a57e38109e6d6a0bf7fe520ed584c6e
Biswajeet Sutar <biswajeetsutar418@gmail.com>
2023-12-14 22:32:29 +0530

    register auth add

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const AuthType = ({ children }: { children: React.ReactElement }) => {
+  return <div>{children}</div>;
+};
+
+export default AuthType;
